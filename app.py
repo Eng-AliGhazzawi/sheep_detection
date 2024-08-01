924d101abcdc61661adc142c62c10e6cd4d443a0
Eng-AliGhazzawi <140108705+Eng-AliGhazzawi@users.noreply.github.com>
2024-08-01 12:33:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -93,8 +93,12 @@ def submit_service():
     # Handle form submission here
     # e.g., saving data to the database
     return redirect(url_for('index.html'))
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
